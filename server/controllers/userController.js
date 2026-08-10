af96e6746cb1cbaa778beae2b7532ce63bdf81bb
= <hb24mcf1r14@student.nitw.ac.in>
2026-08-10 22:44:49 +0530

    modified UserController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -104,6 +104,8 @@ export const updateProfile = async (req, res) => {
     res.json({ success: true, user: updatedUser });
   } catch (error) {
     console.log(error.message);
+    console.log(error.message); 
     res.json({ success: false, message: error.message });
+ 
   }
 };
