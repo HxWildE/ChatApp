91d52cddfff48024e3cff87b1c9902edbbeacb81
= <hb24mcf1r14@student.nitw.ac.in>
2026-09-25 19:15:31 +0530

    chore: update client folder work

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -75,9 +75,7 @@ export const AuthProvider = ({children}) =>{
 		if(!userData || socket?.connected) return;
 		
 		const newSocket = io(backendUrl ,{
-				query: {
-					userId : userData._id,
-				}
+			withCredentials: true
 		});
 
 		newSocket.connect();
